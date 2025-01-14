f981684b177566f8ef7ff415751f352d80b699bb
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-14 16:53:25 +1100

    Amend 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-    language that combines remarkable power with very clear syntax"
+ language that combines remarkable power with very clear syntax"
 word_object = str[39:66] + str[106:111]
 str = word_object + " " + str[:6]
 print(str)
