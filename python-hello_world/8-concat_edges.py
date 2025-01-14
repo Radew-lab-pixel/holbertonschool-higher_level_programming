d25fd04d25440f6fb5b7eb16901b315c01a4d2be
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-14 16:46:07 +1100

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
 word_python = str[:6]
 word_object = str[39:66] + str[106:111]
 str = word_object + " " + word_python
