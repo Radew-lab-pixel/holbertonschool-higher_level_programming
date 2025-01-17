bc42553b62bbb0eddcea3f1e41486c4bd82c03b8
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-17 22:59:30 +1100

    Amend 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
+from add_0 import add
 
-import add_0
 a = 1
 b = 2
-result = add_0.add(a, b)
-print("{}".format(a), "+ {}".format(b), "= {}".format(result), end="")
-print("")
+result = a + b
+print("{}".format(a), "+ {}".format(b), "= {}".format(result))
