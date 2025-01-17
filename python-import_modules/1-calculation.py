2cdcc0336b0d90c1f80cd48d237622d10cdd084c
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-17 23:46:57 +1100

    Add 1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul ,div
+    
+    a = 10
+    b = 5
+
+    result_sum = add(10, 5)
+    print("{}".format(a), "+ {}".format(b), "= {}".format(result_sum))
+    result_sub = sub(10, 5)
+    print("{}".format(a), "- {}".format(b), "= {}".format(result_sub))
+    result_mul = mul(10, 5)
+    print("{}".format(a), "* {}".format(b), "= {}".format(result_mul))
+    result_div = div(10, 5)
+    print("{}".format(a),"/ {}".format(b),"= {}".format(result_div))
+
+
