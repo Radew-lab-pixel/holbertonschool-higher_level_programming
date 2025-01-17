6fe9359ee6cc2ca72d9c2cd92157e76f10c6f020
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-17 12:38:45 +1100

    Amend 7_islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def islower(c):
     if ord(c) >= 97 and ord(c) < 123:
         return True
     else:
         return False
-    #for i in range(97, 123):
-        #ref = chr(i)
+    # for i in range(97, 123):
+        # ref = chr(i)
     #    ref = i
         # print(f"{ref}", end="")
     #    if chr(ref) == c:
