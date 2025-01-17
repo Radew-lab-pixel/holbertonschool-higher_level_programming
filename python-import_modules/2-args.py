b9c9ded376bd97fca9d30b6a03783defa482c901
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-18 01:17:09 +1100

    Amend 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-n = len(sys.argv)
-if (n == 2):
-    print("{} argument:".format(n - 1))
-elif (n == 1):  # zero arguemnt
-    print("{} arguments.".format(n - 1))
-else:
-    print("{} arguments:".format(n-1))
-if n > 1:
-    for i in range(1, n, 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    n = len(sys.argv)
+    if (n == 2):
+        print("{} argument:".format(n - 1))
+    elif (n == 1):  # zero arguemnt
+        print("{} arguments.".format(n - 1))
+    else:
+        print("{} arguments:".format(n-1))
+    if n > 1:
+        for i in range(1, n, 1):
+            print("{}: {}".format(i, sys.argv[i]))
