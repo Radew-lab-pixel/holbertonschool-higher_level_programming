b4ed905b8763ebdb6c83c131f79c3b10c6b26442
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-17 14:45:40 +1100

    Add 11-pow.py

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def pow(a, b):
+    result = a
+    b_copy = b
+    if b_copy == 0:
+        return (1)
+
+    elif b_copy < 0:
+        b = b * -1
+    for i in range(1, b, 1):
+        result = result * a
+    if b_copy < 0:
+        result = 1 / result
+    return (result)
