7c713921fd090346b990367a6a514e8bab4a3de7
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-18 00:09:13 +1100

    Amend 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-"""Print the sum, difference, multiple and quotient of 10 and 5."""
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
 
     from calculator_1 import add, sub, mul, div
 
@@ -16,4 +16,3 @@ if __name__ == "__main__":
     print("{}".format(a), "* {}".format(b), "= {}".format(result_mul))
     result_div = div(10, 5)
     print("{}".format(a), "/ {}".format(b), "= {}".format(result_div))
-
