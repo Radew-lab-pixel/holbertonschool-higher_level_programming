804ecd8c2d09e7de513260bc952679042cc129c7
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-17 14:12:28 +1100

    Pycodes issuew

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-def print_last_digit(number):
 
+def print_last_digit(number):
     if (number < 0):
         modolus = -1 * number % 10
     else:
         modolus = number % 10
-    #return modolus
+    # return modolus
     print("{0}".format(modolus), end="")
     return modolus
