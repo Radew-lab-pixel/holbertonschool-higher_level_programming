9c18248601316e3938930f6c106a2488793b66c6
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-17 13:34:59 +1100

    Amend 8-uppercase.py - pycodestyle error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,10 +3,10 @@ def uppercase(str):
     low_to_up = ord('a') - ord('A')
     # str = "Helo World"
     length = len(str)
-    
-    for i in range (0,length,1):
+
+    for i in range(0, length, 1):
         str_ord = ord(str[i])
-        if (str_ord >= 97 and str_ord <= 122): # lowercase
+        if (str_ord >= 97 and str_ord <= 122):  # lowercase
             str_ord = str_ord - low_to_up
             # str[i] = chr(str_ord) cannot change string variable in Python
         print("{0}".format(chr(str_ord)), end="")
