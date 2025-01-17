9442953741866288c216b80176ce56b343e60d74
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-17 23:49:47 +1100

    Amend 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,8 +2,8 @@
 
 if __name__ == "__main__":
 
-    from calculator_1 import add, sub, mul ,div
-    
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
 
@@ -14,6 +14,5 @@ if __name__ == "__main__":
     result_mul = mul(10, 5)
     print("{}".format(a), "* {}".format(b), "= {}".format(result_mul))
     result_div = div(10, 5)
-    print("{}".format(a),"/ {}".format(b),"= {}".format(result_div))
-
+    print("{}".format(a), "/ {}".format(b), "= {}".format(result_div))
 
