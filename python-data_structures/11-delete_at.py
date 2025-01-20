b082b7e44b36221310bebd4fe1bb9de1cfe764b2
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-21 00:39:04 +1100

    Add 11-delete_at.py

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+
+    if (idx <= 0) or (my_list == []):
+        return (my_list)
+    else:
+        length = len(my_list)
+        result = []
+        for i in range(0, length, 1):
+            if (i != idx):
+                result.append(my_list[i])
+        return (result)
