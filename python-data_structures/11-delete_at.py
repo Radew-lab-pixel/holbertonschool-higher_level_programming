cde56d1dceac40814d3eafb90205644451d663a1
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-21 00:54:59 +1100

    Amend 11-delete_at.py - checker

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@
 
 def delete_at(my_list=[], idx=0):
 
-    if (idx <= 0) or (my_list == []):
+    if (idx < 0) or (my_list == []) or (idx > len(my_list)):
         return (my_list)
     else:
         length = len(my_list)
@@ -12,7 +12,7 @@ def delete_at(my_list=[], idx=0):
             if (i != idx):
                 result.append(my_list[i])
         my_list.clear()
-        for i in range(0, length - 1, 1):
+        for i in range(0, len(result), 1):
             my_list.append(result[i])
         my_list = result
         return (result)
--- python-data_structures/11-main.py
+++ python-data_structures/11-main.py
@@ -6,3 +6,16 @@ idx = 3
 new_list = delete_at(my_list, idx)
 print(new_list)
 print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 0
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 5
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
