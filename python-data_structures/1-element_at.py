52dccc6877f47953b35d153cb9efa788c299a476
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-20 22:34:40 +1100

    Amend 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -16,6 +16,6 @@ def element_at(my_list, idx):
     length = len(my_list)
 
     if (idx < 0 or idx >= length):
-        return("None")
+        return ("None")
     else:
-        return(my_list[idx])
+        return (my_list[idx])
