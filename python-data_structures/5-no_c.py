33b7a14af7fe30b48a2badfd996b48bf9e5dd160
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-20 22:48:55 +1100

    Amend 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -14,8 +14,10 @@
 def no_c(my_string):
 
     # copied_string = my_string
-    # copied_string = my_string.translate({ord('c'): None}) # no working on checker
-    # copied_string = my_string.translate({ord('C'): None}) # on working on checker
+    # copied_string = my_string.translate({ord('c'): None})
+    # no working on checker
+    # copied_string = my_string.translate({ord('C'): None})
+
     # print(copied_string)
     copied_string = my_string.translate({ord(i): None for i in 'cC'})
 
