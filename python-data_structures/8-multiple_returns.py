6aba26af985ecf090b4456916f24a4f92c06e6e7
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-20 23:54:06 +1100

    Add 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,9 +3,13 @@
 
 def multiple_returns(sentence):
 
-    list_sent = list(sentence)
-    first = list_sent[0]
-    length = len(list_sent)
-    if (length == 0):
+    if (sentence == ""):
         first = "None"
+        length = 0
+    else:
+        list_sent = list(sentence)
+        first = list_sent[0]
+        length = len(list_sent)
+        # if (length == 0):
+        # first = "None"
     return (length, first)
