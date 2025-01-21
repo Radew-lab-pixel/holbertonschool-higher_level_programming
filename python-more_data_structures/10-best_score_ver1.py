817cb12a84ac9baffa523fe5222a34b2cf5e1f4a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-22 10:32:40 +1100

    Revert task 10

--- python-more_data_structures/10-best_score_ver1.py
+++ python-more_data_structures/10-best_score_ver1.py
@@ -6,14 +6,14 @@ def best_score(a_dictionary):
     # key_max = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
     # max_value = max(a_dictionary, key = lambda x: x[1])
     # print(max_value)
-    if a_dictionary:
+    if (a_dictionary):
         max_value = max(a_dictionary)
-        if max_value == "Molly":  # stupid checker ??
-            return ("John")
-        elif max_value == "e": #stupid checker ??
-            return ("c")
-        else:
-            return (max_value)
+        # if max_value == "Molly":  # stupid checker ??
+        #    return ("John")
+        # elif max_value == "e": #stupid checker ??
+        #    return ("c")
+        #else:
+        return (max_value)
         # return (max(a_dictionary))
     else:
         return (None)
