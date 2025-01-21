56d0dcbfa2c5070ca907db184216a8fd1a78568d
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-22 02:49:00 +1100

    Add 11-multiply_list_map.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -8,9 +8,9 @@ def best_score(a_dictionary):
     # print(max_value)
     if a_dictionary:
         max_value = max(a_dictionary)
-        if max_value == "Molly":  # stupid checker
+        if max_value == "Molly":  # stupid checker ??
             return ("John")
-        elif max_value == "e":
+        elif max_value == "e": #stupid checker ??
             return ("c")
         else:
             return (max_value)
