7c805f56d8308f07060cd24ed5ab504d9649bc48
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-22 01:10:09 +1100

    Add 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -6,9 +6,9 @@ def best_score(a_dictionary):
     # key_max = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
     # max_value = max(a_dictionary, key = lambda x: x[1])
     # print(max_value)
-    if (a_dictionary is None):
-        return (None)
-    else:
+    if (a_dictionary):
         max_value = max(a_dictionary)
         return (max_value)
+    else:
+        return (None)
     # return (max_value)
