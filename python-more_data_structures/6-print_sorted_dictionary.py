09ca724b16a233df6e31f075c1b5d5630e13a7c3
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-21 21:14:36 +1100

    Add 6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,5 +5,10 @@ def print_sorted_dictionary(a_dictionary):
     # reference from :
     # sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
     sorted_dict = dict(sorted(a_dictionary.items(), reverse=False))
-    print(sorted_dict)
+    # length = len(sorted_dict)
+    # for i in range(0, length, 1):
+    #    print(sorted_dict[i])
+    for key, value in sorted_dict.items():
+        print("{}: {}".format(key, value))
+    # print(sorted_dict)
     # return (sorted_dict)
