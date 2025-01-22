5c6a56006893522a2f1ec39a97b75b4b33d8c4f0
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-23 03:03:26 +1100

    Amend  0-safe_print_list.py - add ValueError checker

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -23,6 +23,6 @@ def safe_print_list(my_list=[], x=0):
                 length += 1
             return (int(length))
             # print("Index Error")
-        except ValueError:
+        except (ValueError, TypeError) as err:
             print("")
             return (0)
