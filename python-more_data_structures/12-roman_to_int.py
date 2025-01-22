97b79e7a9ee9ea9aed5cd2282e285fc3d3c30778
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-22 16:14:08 +1100

    update 12-roman_to_int.py - pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,8 +9,9 @@ def check_if_string(input_list, roman_list, length):
         else:
             return (1)
 
+
 def roman_to_int(roman_string):
-    
+
     if roman_string and isinstance(roman_string, str):
         roman_list = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100}
         roman_list.update({'D': 500, 'M': 1000})  # check say too long
@@ -20,7 +21,7 @@ def roman_to_int(roman_string):
 
         if (check_if_string(input_list, roman_list, length)) != 1:
             return (0)
-       
+
         total_value = 0
         # prev_list_value = roman_list.get(input_list[0])
         # print("Prev_list : {}".format(prev_list_value))
@@ -44,9 +45,9 @@ def roman_to_int(roman_string):
                     total_value -= roman_list_value  # subtract
                 else:
                     total_value += roman_list_value
-            else: # it's end/last dict key
+            else:  # it's end/last dict key
                 total_value = total_value + roman_list_value
             # prev_list_value = roman_list.get(input_list[i])
         return (total_value)
-    else: # !roman_string
+    else:  # !roman_string
         return (0)
