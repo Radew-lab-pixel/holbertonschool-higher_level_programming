ed53d4c10ddf3bda64bed8c3f1b9ea198e467276
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-23 02:47:13 +1100

    Add 0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+
+    if (my_list != []):
+        try:
+            new_list = []
+            # lambda (x : length if x > length)
+            # limit = lambda x, length: x if x < length else length
+            for i in range(0, x, 1):
+                new_list.append(my_list[i])
+            # new_string = ''.join(new_list)
+            new_string = ''.join([str(s) for s in new_list])
+            print(int(new_string))
+            return (int(i + 1))
+        except IndexError:
+            # new_string = ''.join(my_list)
+            new_string = ''.join([str(s) for s in my_list])
+            print(int(new_string))
+            length = 0
+            for i in new_string:
+                length += 1
+            return (int(length))
+            # print("Index Error")
