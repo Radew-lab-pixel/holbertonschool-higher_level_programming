76073e5e394fca77a351dc2b08cf597dcc260cda
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-23 14:08:05 +1100

    Add 1-safe_print_integer/py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,8 +5,8 @@ def safe_print_integer(value):
 
     try:
         # result = isinstance(value, int)
-        # print ("{.d}".format(result))
-        print("{.d}".format(value))
+        # print ("{:d}".format(result))
+        print("{:d}".format(value))
         # return (True)
     except Exception as e:
         # except:
