8afc4b0f568cb8e2091e37080a09785e91b2020d
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-24 21:33:18 +1100

    Add 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,7 +2,17 @@
 
 
 def add_integer(a, b=98):
+    
+    """
+    add_integer - module to add two arguments
 
+    Args: 
+        a : integer
+        b : integer
+    
+    Returns:
+        sum : integer of a + b
+    """
     if not isinstance(a, (int, float)):
         # raise Exception("a must be an integer")
         raise TypeError("a must be an integer")
