ed6f450070615683e848510ad81ea41760f15df7
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-25 15:04:01 +1100

    Add 5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 5-text_indentation.py """
+
+
+def text_indentation(text):
+    """
+    function prints a text with 2 new lines after each of these 
+    characters: ., ? and :
+    
+    
+    Args: 
+    text : string input
+    
+    Returns: 
+    None
+    """
+    
+    if not isinstance(text, str):
+        raise TypeError("Text must be a string")
+
+    text_list = list(text)
+    length = len(text_list)
+
+    ref_char = [".", "?", ":"]
+
+    for i in range(0, length,1):
+        print("{}".format(text_list[i]), end="")
+        if text_list[i] in ref_char:
+            # print()  # print two newline
+            # print()
+            print("\n\n")
+            if (text_list[i + 1] == " "):  # next char is space
+                text_list[i+1] = ""  # remove space
+
