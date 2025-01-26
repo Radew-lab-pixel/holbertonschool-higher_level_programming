ef45c158fef88d461e8024372f83ba264d6f695d
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-26 16:56:27 +1100

    Amend 6-max_integer_test.py - add more unit test cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,11 +5,15 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    
+    def setUp(self):
+        self.testListNone = None
 
-    def test_type(self):
 
-        with self.assertRaises(TypeError):
-            max_integer([], 0)
+    # def test_type(self):
+
+    #    with self.assertRaises(AssertionError):
+    #        max_integer([])
     
     def test_max_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
@@ -25,3 +29,14 @@ class TestMaxInteger(unittest.TestCase):
     
     def test_max_all_neg(self):
         self.assertEqual(max_integer([-2, -6, -1, -3, -5]), -1)
+
+    def test_max_only_one(self):
+        self.assertEqual(max_integer([3]), 3)
+
+    def test_max_none(self):
+        # self.assertEqual(max_integer(), 0)
+        # with self.assertRaises(AssertionError):
+            # max_integer()
+            # self.testListNone[:1]
+            # self.assertEqual("'NoneType' object is not subscriptable", str(err.exception))
+        self.assertEqual(max_integer(), None)
