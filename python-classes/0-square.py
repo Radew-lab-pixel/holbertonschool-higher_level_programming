79d1a1630a237860fd89bb31dbb9d3d396992c8a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-28 10:55:55 +1100

    Amend 0-sqaure.py - add class documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,6 +3,12 @@
 
 
 class Square:
+    """ Square class object
+
+        Attributes:
+        self : custom class used to access the class’s members and
+        methods, as well as to create new members
+    """
     def __init__(self):
         # attributes below like structure in c, dict in python
         # self.size = 0
