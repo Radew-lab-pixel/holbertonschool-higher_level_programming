9c68fe7398d5a43cea87338a3185094994dfd75a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-29 10:29:24 +1100

    Add 2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""0-rectangle.py"""
+
+
+class Rectangle:
+    """ Class of Rectangle object """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        # pass
+
+    @property
+    def width(self):  # Getter for width
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):  # setter method
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):  # Getter for height
+        return self.__height
+
+    @height.setter
+    def height(self, value):  # setter for height
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):  # public method to cal area
+        return (self.__width * self.__height)
+
+    def perimeter(self):  # public method to cal perimeter
+        if (self.__width == 0) or (self.__height == 0):
+            return 0
+        else:
+            return (2 * (self.__width + self.__height))
