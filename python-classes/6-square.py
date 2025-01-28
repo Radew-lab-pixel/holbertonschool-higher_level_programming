a86b2428b7a335fb1a78797d5acf9a18f266b02e
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-28 23:11:46 +1100

    Amend 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -73,7 +73,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             # print("")
-            print()
+            print("\n")
         # for m in range(0, self.__position[1]):
         # [print("") for i in range(0, self.__position[1])]
         # for m in range(0,self.__position[1], 1):
