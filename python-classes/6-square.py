6806fb64cb797317fea0019f7496e7c7b93fd552
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-28 20:36:28 +1100

    Amend 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -44,6 +44,9 @@ class Square:
         if not isinstance(list(value)[0], int):
             # print("value[`]")
             raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(list(value)[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
         if not isinstance(value, tuple) and map(lambda x: x < 0, value):
             raise TypeError("position must be a tuple of 2 positive integers")
         # if not value[1]:
