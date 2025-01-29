120d1b744e4d7476685e225172076f7c69088e6a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-29 19:32:46 +1100

    Add 3-rectangle.py

---
+++ python-more_classes/3-rectangle_ver1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""0-rectangle.py"""
+
+
+class Rectangle:
+    """ Class of Rectangle object """
+    # print_area(self)
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height         
+        # pass
+    # print_area(self)
+
+    @property
+    def width(self):  # Getter for width
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):  # setter method
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):  # Getter for height
+        return self.__height
+
+    @height.setter
+    def height(self, value):  # setter for height
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):  # public method to cal area
+        return (self.__width * self.__height)
+
+    def perimeter(self):  # public method to cal perimeter
+        if (self.__width == 0) or (self.__height == 0):
+            return 0
+        else:
+            return (2 * (self.__width + self.__height))
+        
+    def __str__(self):
+        for i in range(0, self.__height, 1):
+            for j in range(0, self.__width, 1):
+                print("#", end="")
+            print()
+
+    # def __repr__(self):
+        # return repr(self)
+    #    result = self.__width + self.__height
+    #    return result
+        # pass
