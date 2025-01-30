c9dd4c1179ed550d05fa8cd690ecf0e2e688e0c2
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-30 14:32:41 +1100

    Add 8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""6-rectangle.py"""
+"""7-rectangle.py"""
 
 
 class Rectangle:
     """ Class of Rectangle object """
     # print_area(self)
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         # number_of_instances += 1
         Rectangle.number_of_instances += 1
-        # pass
+        # Rectangle.print_symbol = "#"
+        # self.print_symbol = "#"
+        # self.print_symbol = "#"
     # print_area(self)
 
     @property
@@ -57,8 +60,10 @@ class Rectangle:
         for i in range(0, self.__height, 1):
             for j in range(0, self.__width, 1):
                 # print("#", end="")
-                result.append("#")
-            # print()
+                # result.append("#")
+                # result.append(str(self.__print_symbol))  # default is #
+                # result.append(Rectangle.print_symbol)
+                result.append(self.print_symbol)
             if i != (self.__height - 1):
                 result.append("\n")
         # return ''.join(result)  # convert to string
@@ -71,7 +76,19 @@ class Rectangle:
         # for you, making the code much simpler and easier to read.
         return f"Rectangle({self.__width}, {self.__height})"
 
-    def __del__(self):  # built in del / destroy
+    def __del__(self):  # del / destroy
         print(f"Bye rectangle...")
         Rectangle.number_of_instances -= 1
-        # self.number_of_instances -= 1
+
+    # def print_symbol(self, value):  # set the symbol
+        # self.__print_symbol = value
+        # Rectangle.print_symbol = value
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 murst be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area == rect_2.area:
+            return rect_1
