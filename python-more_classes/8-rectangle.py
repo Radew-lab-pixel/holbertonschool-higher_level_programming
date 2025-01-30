e9a55ed95fec97d377a4495bbd5134533b55fe84
Radew-lab-pixel <ktmqliu@gmail.com>
2025-01-30 14:37:23 +1100

    Amend 8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -90,5 +90,10 @@ class Rectangle:
             raise TypeError("rect_1 murst be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area == rect_2.area:
+        if rect_1.area() == rect_2.area():
             return rect_1
+        else:
+            if rect_1.area() > rect_2.area():
+                return rect_1
+            else:
+                return rect_2
