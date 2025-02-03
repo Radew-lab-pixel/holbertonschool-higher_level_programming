71388cb5cfa48d14a0360c9e0ba5ccf645b174ef
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-04 01:03:36 +1100

    Amend 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -10,8 +10,8 @@ def is_same_class(obj, a_class):
     Returns:
         True if obj match a_class datatype
     """
-    if isinstance(obj, a_class):
-        return (True)
-    else:
-        return (False)
-
+    # if isinstance(obj, a_class):
+    #    return (True)
+    # else:
+    #    return (False)
+    return isinstance(obj, a_class)
