e1b98335cd52ce4ba165fc477300328e70a46b73
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-03 15:45:10 +1100

    Amend 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ method that return a list of attributes
 and methods of an object """
+
+
 def lookup(obj):
     """ lookup : method that return a list of attributes """
-    """ 
+    """
     Args:
         obj : input
     """
