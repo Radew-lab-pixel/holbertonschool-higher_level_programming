d0af761ba17728165f165adadbefcc400119b26b
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-03 17:24:48 +1100

    Add 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""class inheritance from list"""
-
 
 class MyList(list):   # inherited from list
+    """class inheritance from list"""
     def print_sorted(self):
         """public instant method"""
         # print(print_sorted(self))
