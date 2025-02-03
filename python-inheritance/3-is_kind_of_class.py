07fccf5b15934cc44a12c8352575408a1dd87d0a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-04 01:48:59 +1100

    Add 3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""is_kind_of_class"""
+def is_kind_of_class(obj, a_class):
+    """
+    is_kind_of_class - check if obj is class of a_class
+    Args:
+        obj : input
+        a_class : reference class
+    """
+    return isinstance(obj, a_class)
