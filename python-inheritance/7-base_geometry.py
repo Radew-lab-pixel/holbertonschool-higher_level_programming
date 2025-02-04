1f16ecacb2d3d0b2fb58a41ff00f2af231187987
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-04 11:16:24 +1100

    Add 7-base_geomtry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """
+        area - method
+        Args:
+            self - instance of BaseGeometry
+        Returns:
+            "[Exception] area() is not implemented"
+        """
+        return ("[Exception] area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        integer_validator - public method that validate value
+        Args:
+            self - instance of BaseGeometry
+            name - input string attribute
+            value - input value
+        Returns:
+            True if value is integer else Raise Exception
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        else:
+            return True
