4417b2da8fbf47a038ba3ff625b3c9ab2af417d2
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-04 22:07:20 +1100

    Amend 8-rectangle.py remove super().__init__()  for task 10)

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -12,7 +12,8 @@ class Rectangle(BaseGeometry):
             weight (int): The height of the rectangle.
         """
         # Rectangle.integer_validator("width", width)
-        super().__init__()  # Call the superclass __init__ method
+        # remove for task 10
+        # super().__init__()  # Call the superclass __init__ method
         self.integer_validator("width", width)
         # self.width = width
         self.__width = width  # private attribute
