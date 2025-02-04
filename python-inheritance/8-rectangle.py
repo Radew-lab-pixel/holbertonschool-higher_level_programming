8b61ddd5166b9780c0e40a25cfbca8483660b137
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-04 15:18:09 +1100

    Amend 8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,37 +1,6 @@
 #!/usr/bin/python3
 """ BaseGeometry class"""
-
-
-class BaseGeometry:
-    """BaseGeometry class"""
-    def area(self):
-        """
-        area - method
-        Args:
-            self - instance of BaseGeometry
-        Returns:
-            "[Exception] area() is not implemented"
-        """
-        raise Exception("area() is not implemented")
-        # return ("[Exception] area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        integer_validator - public method that validate value
-        Args:
-            self - instance of BaseGeometry
-            name - input string attribute
-            value - input value
-        Returns:
-            True if value is integer else Raise Exception
-        """
-        # if not isinstance(value, int): not working in Task 8
-        if not (type(value) == int):
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        else:
-            return True
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
