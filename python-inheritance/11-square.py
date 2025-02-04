ad0bab19b1c04b2abe59a66ef23c85ec6bd4b743
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-04 22:21:57 +1100

    Amend 10-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" child class of Square inherited from Rectangle """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """child Square inherited from Rectangle"""
+    def __init__(self, size):
+        """ Initialize a Square instance.
+            Args:
+                size (int): The width of the Square.
+        """
+        # super().__init__(size, size)  # initalize the parent instance
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        # self.__width = size
+        # self.__height = size
+        self.__size = size
+
+    def area(self):
+        """"public method for calculating area"""
+        return self.__size * self.__size
+        # return self.__width * self.__height
+
+    def __str__(self):
+        """"""
