aea1b5404bd1ca6df49836a2c67ca6607bbfdef7
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-04 15:25:25 +1100

    Amend 7-base_geometry.py - use type instead of instnace for task 8

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -26,7 +26,7 @@ class BaseGeometry:
             True if value is integer else Raise Exception
         """
         # if not isinstance(value, int):  # not working in Task 8
-        if not (type(value) == int):  # work in Task 8
+        if (type(value) != int):  # work in Task 8
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
