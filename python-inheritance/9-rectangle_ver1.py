146b3a0dcd109edf8d0203c2846983de99ab9e82
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-05 22:25:27 +1100

    Amend "task_02_verboselist.py" remove from sympy.codegen.ast import continue_

---
+++ python-inheritance/9-rectangle_ver1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" BaseGeometry class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """child class of BaseGeometry"""
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance.
+        Args:
+            width (int): The width of the rectangle.
+            weight (int): The height of the rectangle.
+        """
+        # Rectangle.integer_validator("width", width)
+        super().__init__()  # Call the superclass __init__ method
+        self.integer_validator("width", width)
+        # self.width = width
+        self.__width = width  # private attribute
+        self.integer_validator("height", height)
+        # self.height = height
+        self.__height = height  # private attribute
+
+    def area(self):  # public method
+        """public method to cal area"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """return human-readable string representation of an object"""
+        # result = []
+        result = f"[Rectangle] {self.__width}/{self.__height}"
+        return (result)
