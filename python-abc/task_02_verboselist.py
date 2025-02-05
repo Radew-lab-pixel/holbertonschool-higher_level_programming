dbcbeae7d7a0a7ca78b0b260dba1e59e07674329
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-05 17:19:51 +1100

    Amend "task_02_verboselist.py"

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""class VerboseList inherits from list"""
+from sympy.codegen.ast import continue_
+
+
+class VerboseList(list):
+    """VerboseList class inherit from list"""
+    def __init__(self, iterable):
+        super().__init__(str(item) for item in iterable)
+
+    def append(self, item):
+        """child method named append"""
+        super().append(item)
+        print(f"Added [{item}] to the list")
+
+    def extend(self, other):
+        """child method named extend"""
+        if isinstance(other, type(self)):
+            super().extend(other)
+        else:
+            super().extend(item for item in other)
+        print(f"Extended the list with {len(other)} items.")
+
+    def remove(self, item):
+        """child method named remove"""
+        print(f"Removed [{item}] from the list.")
+        try:
+            super().remove(item)
+        except ValueError:
+            print("", end="")
+
+    def pop(self):
+        print(f"Popped [item] from the list.")
+        super().pop()
+
+    # def pop(self, item):
+        # """child method named pop"""
+        # print(f"Popped [item] from the list.")
+        # super().pop(item)
+
+    def pop(self, index=-1):
+        """child method named pop"""
+        result = super().pop(index)
+        print(f"Popped [{result}] from the list")
+        return result
