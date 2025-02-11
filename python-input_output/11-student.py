a7b0ada71b5384bc769c523ca567d2391ddbadbd
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-11 22:33:37 +1100

    Add 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""class Student that defines a student by"""
+
+
+class Student:
+    """constructor method"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """method that return dictionary representation of a Student instance
+        to_json - method
+        argv :
+            self - object passed in
+            attrs - list of strings,
+        Return :
+            self.__dict__   - if attrs is None, return dict representation
+                               of all student instances
+                            - else only student instances listed in attrs
+    """
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        # if self.__dict__.items()
+            # Create an empty dictionary to store the filtered attributes
+        filtered_dict = {}
+
+        # Loop through all the attributes of the instance
+        for key, value in self.__dict__.items():
+            # Check if the current attribute is in the 'attrs' list
+            if key in attrs:
+                filtered_dict[key] = value
+        return filtered_dict
+
+    """public method that replaces all attributes of the Student instance"""
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            # self.__dict__.key = value
+            setattr(self, key, value)
+            # setattrsets the value of the attribute of an object.
