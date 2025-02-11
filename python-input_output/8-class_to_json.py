2bce0e14ca25ff14f2686a8f61c4c1e66a9d85bf
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-11 17:58:09 +1100

    Add 8-my_class.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+"""function that returns the dictionary description with simple data structure"""
+import json
+
+
+def class_to_json(obj):
+    # text = dict(obj)
+    # result = json.loads(obj)
+    # return result
+    # result = dict(obj)
+    # return result
+    return obj.__dict__
