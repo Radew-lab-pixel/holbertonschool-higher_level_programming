c50377a0a540aac0ac97d1c381399bd880948853
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-15 11:37:04 +1100

    Amend task_02_csv.py - add try and except

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -15,22 +15,29 @@ def convert_csv_to_json(filename_csv):
     data_list = [] # create a list
 
     # step 2 open csv file handler
-    with open(filename_csv, 'r', encoding='utf-8') as file_csv:
-        # read file_csv to csv_reader pior to convert to dict
-        csv_reader = csv.DictReader(file_csv, delimiter=',')
-        # convert each row into a dict and add converted data
-        # to data variable
-        for rows in csv_reader:
-            # column named 'name' in data.csv is the primary key
-            # 1st row is header
-            # key = rows['name']  # 1st row 1st column is 'name'
-            # data_dict[key] = rows  # auto assign value to resective key
-            data_list.append(rows)  # Append entire row to the list
+    try:
+        with open(filename_csv, 'r', encoding='utf-8') as file_csv:
+            # read file_csv to csv_reader pior to convert to dict
+            csv_reader = csv.DictReader(file_csv, delimiter=',')
+            # convert each row into a dict and add converted data
+            # to data variable
+            for rows in csv_reader:
+                # column named 'name' in data.csv is the primary key
+                # 1st row is header
+                # key = rows['name']  # 1st row 1st column is 'name'
+                # data_dict[key] = rows  # auto assign value to resective key
+                data_list.append(rows)  # Append entire row to the list
+    except Exception as err:
+        print("")
 
     # open a json file handler and use json dumps
     # to convert dict to json string
     filename_json = "data.json"
-    with open(filename_json, 'w', encoding ='utf-8') as file_json:
-        # step 3 : json dumps to convert dict to json string
-        # file_json.write(json.dumps(data_dict, indent=4))
-        file_json.write(json.dumps(data_list, indent=4))
+    try:
+        with open(filename_json, 'w', encoding ='utf-8') as file_json:
+            # step 3 : json dumps to convert dict to json string
+            # file_json.write(json.dumps(data_dict, indent=4))
+            file_json.write(json.dumps(data_list, indent=4))
+            return True
+    except Exception as err:
+        return False
