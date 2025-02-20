99cac1f771df95719990a6fe0f7fe10a1ee044d5
Radew-lab-pixel <ktmqliu@gmail.com>
2025-02-20 18:18:43 +1100

    Amend task_03_http_server.py - add urllib.parse , json.dumps(data).encode())

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,12 +4,18 @@
     Handle different types of HTTP requests (GET, POST, etc.).
     Serve JSON data in response to specific endpoints.
 """
-#import http.server
-#import socketserver
+import http.server
+import socketserver
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+from urllib.parse import urlparse, parse_qs
 
+data = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
 
 """basic webserver"""
 def webserver_basic():
@@ -31,8 +37,30 @@ class simpleHTTPServer(BaseHTTPRequestHandler):
     def do_GET(self):
         """Handle HTTP GET request"""
         self.send_response(200)
+        # send header
+        # self.send_header("Content-Type", "application/json;charset=UTF-8")
+        
         self.end_headers()
+        # response by stdout Hello ... 
         self.wfile.write(b'Hello, this is a simple API!')
+        # respond by stdout data in json format
+        # json.dump(data, self.wfile) # has to be in post
+
+        """Handle GET requests to parse url address"""
+        parsed_path = urlparse(self.path)
+        
+        if parsed_path.path == '/data':
+            # self.handle_json_data response()
+            print("endpoint /data parsed")
+            self.wfile.write(b"\nendpoint /data parsed\n")
+            self._json_data_response()
+
+    def _json_data_response(self):
+        """You should return a simple dataset"""
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
 
 httpd = HTTPServer(('', 8000), simpleHTTPServer)
 httpd.serve_forever()
