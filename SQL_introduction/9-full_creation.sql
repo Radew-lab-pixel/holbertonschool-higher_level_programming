cba71537cedc6f1cfeca07397301beffa2be5e5c
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-06 19:09:58 +1100

    Amend 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- 9-full_creation.sql
 CREATE TABLE IF NOT EXISTS second_table
     (id INT, name VARCHAR(256), score INT)
-INSERT INTO second_tables VALUES
+INSERT INTO second_table VALUES
     (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
