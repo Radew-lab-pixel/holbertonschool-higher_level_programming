e582798bc530b9cbc856c6811c97389c05bd168a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-08 23:09:14 +1100

    Amend 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,5 @@
 -- 2-create_read_user.sql
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
