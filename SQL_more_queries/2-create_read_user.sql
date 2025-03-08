3dd731c4b34557f0c14fdea3a67711a2a7802824
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-08 22:14:28 +1100

    Amend 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- 2-create_read_user.sql
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 USE htbn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS user_0d_2@localhost;
 ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT ALL PRIVILEGES ON htbn_0d_2 TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
