032bb189c811faa64910b0225cca044ff13ff403
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-08 17:36:01 +1100

    Add [200~1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 1-create_user.sql
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANTS ALL PRIVILEDGES;
+FLUSH PRIVILEDGES;
