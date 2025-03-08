46961c83d422b77be5c42409a6a5e07e2a05f32b
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-08 17:05:52 +1100

    update

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- 0-privileges.sql
--- CREATE USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'user_0d_1'@localhost';
 SHOW GRANTS FOR 'user_0d_2'@localhost';
