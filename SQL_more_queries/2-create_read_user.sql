27bd6535436ff981075373308dc0c8af613ab93a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-08 22:44:20 +1100

    Amend 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,8 +2,11 @@
 -- mysql -u root -p < 2-create_read_user.sql
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 USE htbn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE USER 
+    IF NOT EXISTS 'user_0d_2'@'localhost' 
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON htbn_0d_2.* 
+    TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
 -- REVOKE ALL PRIVILEGES ON htb_0d_2.* FROM USER 'user_0d_2'@'localhost'; not working
 -- REVOKE ALL PRIVILEGES ON hbtn_0d_2.* FROM 'user_0d_2'@'localhost';
