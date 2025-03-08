3f88e5f4d15848a1eb0f997ada64bd93ff72255a
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-08 23:39:34 +1100

    Add 6-sates.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- 6-states.sql
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT PRIMARY KEY NOT NULL UNIQUE,
+    name VARCHAR(256) NOT NULL);
