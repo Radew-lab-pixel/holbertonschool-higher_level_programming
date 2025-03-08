7f323a8e7d3a03f770b3c2582d96213e10e841c2
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-08 22:46:55 +1100

    Amend 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 USE htbn_0d_2;
 CREATE USER 
-    IF NOT EXISTS 'user_0d_2'@'localhost' 
+    IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON htbn_0d_2.* 
     TO 'user_0d_2'@'localhost';
