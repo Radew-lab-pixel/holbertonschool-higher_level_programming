e6cdb8ac7ab0c852f310e8345b78677d453adf76
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-10 01:08:33 +1100

    Amend 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- 10-genre_id_by_show.sql
 -- LOAD DATABASE INFILE 'hbtn_0d_tvshows' TO DATABASE hbtn_0d_tvshows; cannot use on the sql dump
 -- CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;  removed as failed checker but works on terminal
--- USE hbtn_0d_tvshows; removed as failed checker but works on terminal
+USE hbtn_0d_tvshows; removed as failed checker but works on terminal
 SELECT tv_shows.title,
     tv_show_genres.genre_id
 FROM tv_shows
