f98703675b272bbb28042aa0a39563a50fb3ff6b
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-10 00:53:26 +1100

    Amend 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,6 @@
 -- 10-genre_id_by_show.sql
--- LOAD DATABASE INFILE 'hbtn_0d_tvshows' TO DATABASE hbtn_0d_tvshows; cannot use on the sql dump
 CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
--- USE hbtn_0d_tvshows;
+USE hbtn_0d_tvshows;
 SELECT 
     tv_shows.title,
     tv_show_genres.genre_id
