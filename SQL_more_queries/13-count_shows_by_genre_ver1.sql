754e261979f511b9e29c23e1ff8b4a780e8917dc
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-10 14:50:08 +1100

    Add 13--count_shows_by_genre.sql

---
+++ SQL_more_queries/13-count_shows_by_genre_ver1.sql
@@ -0,0 +1,16 @@
+-- 13-count_shows_by_genre.sql
+-- Lists genres and the number of linked shows, sorted by show count in descending order
+SELECT 
+    tv_genres.name AS genre,
+    COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM 
+    tv_genres
+INNER JOIN 
+    tv_show_genres 
+ON 
+    tv_genres.id = tv_show_genres.genre_id
+GROUP BY 
+    tv_genres.name
+ORDER BY 
+    number_of_shows DESC;
+
