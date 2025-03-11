2363ca91d87c45635562f648e39213e0d81ff94f
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-11 22:42:40 +1100

    Add 5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Write a script that lists all cities by states
+    from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # establish a connection
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         password=mysql_password,
+                         database=database_name)
+
+    # cursor pointer
+    pointer = db.cursor()
+
+    # execute a query
+    pointer.execute("SELECT cities.name FROM cities\
+                    JOIN states ON cities.state_id = states.id\
+                        WHERE binary states.name = %s\
+                    ORDER BY cities.id", (state_name,))
+
+    # fetch result from pointer
+    rows = pointer.fetchall()
+    for row in rows:
+        print(f"{row[0]}", end=" ")
+    print()
+    # close pointer and db
+    pointer.close()
+    db.close()
