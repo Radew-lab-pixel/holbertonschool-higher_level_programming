d5d9689d75e74a003b55d1ab4ac57b1b4bd466b9
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-11 19:03:33 +1100

    Amend

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,42 +1,26 @@
 #!/usr/bin/python3
-"""Write a script that lists all states from the database hbtn_0e_0_usa: """
-import MySQLdb
-import sys
+""" Nameless module for running SQL """
 
-def main():
-    # read argumnent
-    n = len(sys.argv)
-    """if n != 3:
-    print("Arguments number need to be 3")
-    exit()
-    """
-    mysql_username = sys.argv[0]
-    mysql_password = sys.argv[1]
-    database_name = sys.argv[2]
-    # Establish a connection
-    db = MySQLdb.connect(host="localhost", port=3306,
-        user=mysql_username, password=mysql_password, database=database_name)
-
-    # create a cursor pointer
-    pointer = db.cursor()
-
-    # execute a query
-    pointer.execute("SELECT * FROM states ORDER BY id ASC")
-
-    """ # fetch the result (fetchone fetch the next row)
-    row = pointer.fetchone()
-    while row is not None:
-        print(row)
-        row = pointer.fetchone()"
-    """
-
-    # fetch the result using fetchall to pass the checker
-    rows = pointer.fetchall()
-    for row in rows:
-        print(row)
-    # close cursor pointer and db connection
-    pointer.close()
-    db.close()
 
 if __name__ == "__main__":
-    sys.exit(main())
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id ASC""")
+    rows = c.fetchall()
+
+    for row in rows:
+        print("({0}, '{1}')".format(row[0], row[1]))
+
+        # print("(%s, " % row[0], end = "")
+        # print("%s)" % row[1])
+        
