0c63125810fe5cab82f23baa1da9757f1a0ee3fd
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-12 00:19:45 +1100

    Add model-state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,12 +2,13 @@
 """Write a python file that contains the class definition of
 a State and an instance Base = declarative_base()
 """
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# instance base from Declarative base
 Base = declarative_base()
 
+# class State inherited from Base
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True)
