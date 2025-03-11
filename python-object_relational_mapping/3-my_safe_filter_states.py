33efbc485f8a7d0a72ca2fbf362975394f08a94e
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-11 21:37:46 +1100

    Add 3-my_safe filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Write a script that takes in an argument and displays
 all values in the states table of hbtn_0e_0_usa where
-name matches the argument.
-But this time, write one that is safe from MySQL injections!
-Same as file same as task 2
-"""
+name matches the argument."""
 
 import MySQLdb
 import sys
