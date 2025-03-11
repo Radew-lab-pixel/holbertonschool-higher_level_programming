24520f322bccba0ca4607daa9b53cfbad9047868
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-11 22:27:19 +1100

    Add 4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Write a script that lists all cities
+    from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # establish a connection
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         password=mysql_password,
+                         database=database_name)
+
+    # cursor pointer
+    pointer = db.cursor()
+
+    # execute a query
+    pointer.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                    JOIN states ON cities.state_id = states.id")
+
+    # fetch result from pointer
+    rows = pointer.fetchall()
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}', '{row[2]}")
+
+    # close pointer and db
+    pointer.close()
+    db.close()
