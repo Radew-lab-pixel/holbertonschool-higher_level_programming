e4a34e0b62dfcbc3c53a81c7b8377a5d5c05e9c8
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-13 03:52:01 +1100

    Add 10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Write a script that prints the State object with the name passed
+    as argument from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, URL
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import State
+
+if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_to_search = sys.argv[4]
+    port = 3306
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format
+                           (mysql_username, mysql_password, port,
+                            database_name), pool_pre_ping=True)
+
+    # start ORM engine
+    Session = sessionmaker(bind=engine)
+    session = Session()  # start a session
+
+    results = session.query(State).filter(
+        State.name.like(state_name_to_search))
+
+    if results:
+        for result in results:
+            print(f"{result.id}")
+    else:
+        print("Not found")
