ca68d86627f9ec5858f657a134468f3a46bf8ede
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-01 15:19:46 +1100

    Add task_02_jinga.py

--- python-server_side_rendering/task_02_jinga.py
+++ python-server_side_rendering/task_02_jinga.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+import json
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
+# static module for loading json file
+def read_file():
+    filename = "items.json"
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data['items']  # return dict values (alist) of key items
+
 
 @app.route('/')
 def home():
@@ -14,6 +24,13 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    items_list = read_file()
+    return render_template('items.html', items=items_list)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
