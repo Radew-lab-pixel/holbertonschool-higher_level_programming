a00b54020d1f97b36a7ed54fde04f7dae4e5bbe4
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-02 15:14:03 +1100

    update

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,11 @@
+function setHeaderColor() {
+    const urlParams = new URLSearchParams(window.location.search);
+    // const source = urlParams.get('source');
+    // const header = document.getElementById('pageHeader');
+    const header = document.querySelector('header')  // using querySelctor
+    
+    header.style.color = '#FF0000';  // For invalid/unknown sources
+    }
+
+// Wait for DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', setHeaderColor);
